61b4e7957e3a746d31fa3bfa41b808a81c27649e
Kohansol <64995062+khs-st@users.noreply.github.com>
2020-05-29 14:58:05 +0900

    7번 해결

--- Project0529/Project0529/연습문제0529_2.c
+++ Project0529/Project0529/연습문제0529_2.c
@@ -13,7 +13,7 @@ double get_area_of_square(double length) {
 	return area_of_square;
 }
 */
-/*3번
+
 double distance(int x1, int y1, int x2, int y2) {
 	int a, b, c;
 	a = (x2 - x1);
@@ -22,7 +22,7 @@ double distance(int x1, int y1, int x2, int y2) {
 	double distance = sqrt(c);
 	return distance;
 }
-*/
+
 /*4번
 int discount_price(double d, int p) {
 	double rate;
@@ -33,32 +33,46 @@ int discount_price(double d, int p) {
 }
 */
 /*5번
-int is_even(even) {
-	even++;
-	return even;
+int is_even(num) {
+	
+	if (num % 2 == 0 && num != 0) {
+		return 3;
+	}
 }
 
-int is_odd(odd) {
-	odd++;
-	return odd;
+int is_odd(num) {
+	if (num % 2 == 1 && num != 0) {
+		return 4;
+	}
 }
-
-
 */
 /*6번
-int choose_menu(menu) {
-	if (menu == 1)
+
+int choose_menu(num) {
+	if (num == 1)
 		printf("파일 열기를 수행합니다.\n");
-	else if (menu != 1)
+	else if (num != 1)
 		printf("");
 	return 1;
 }
 */
-/*7번*/
-int is_prime(int num) {
-		num++;
-	return num;
+/*7번
+int is_prime(int N) {
+	int i = 0, prime = 0, count = 0;
+	for (prime = 2; prime <= N; prime++)
+	{
+		for (i = 2; i <= prime; i++)
+			if (prime % i == 0) break;
+		if (prime == i)
+		{
+			printf(" %d", prime);
+			count++;
+		}
 	}
+
+	return count;
+}
+*/
 
 
 
@@ -82,14 +96,14 @@ int main() {
 		printf("정사각형의 면적: %.6lf", get_area_of_square(l));
 		printf("\n\n");
 		*/
-		/*3번
+		
 				int x1, y1, x2, y2;
 				printf("직선의 시작점 좌표? ");
 				scanf("%d %d", &x1, &y1);
 				printf("직선의 끝점 좌표? ");
 				scanf("%d %d", &x2, &y2);
 				printf("(%d,%d)~(%d,%d) 직선의 길이: %lf",x1,y1,x2,y2, distance(x1,y1,x2,y2));
-				*/
+				
 				/*4번
 
 					double d;
@@ -105,22 +119,24 @@ int main() {
 						printf("할인가:%d원 ", discount_price(d,p));
 					}
 				*/
-				/*5번
-	int num, i = 0, j = 0, even = 0, odd = 0;
-
+	/*5번
+	int num, even = 0, odd = 0;
 
 	printf("정수를 빈칸으로 구분해서 입력하세요.(마지막에 0 입력)\n");
 	while (1)
 	{
 		scanf("%d", &num);
 		if (num == 0) break;
-		else if (num % 2 == 0)
-
-			even = is_even(even);
-		else
-			odd = is_odd(odd);
+		if (is_even(num) == 3)
+		{
+			even++;
+		}
+		if (is_odd(num) == 4)
+		{
+			odd++;
+		}
 	}
-	printf("입력받은 정수 중 짝수는 %d개, 홀수는 %d개입니다.", even, odd);
+	printf("입력받은 정수 중 짝수는 %d개, 홀수는 %d개입니다.",even , odd);
 	*/
 
 	/*6번
@@ -132,24 +148,20 @@ int main() {
 	scanf("%d", &menu);
 	if (menu == 0)
 		break;
-	menu = choose_menu(menu);
+	if(choose_menu(menu)==1);
 
 	}
 	*/
-	/*7번*/
-	int primenumber, N, num = 0;
+	/*7번
+	int prime, N, count = 0;
 
 	printf("1~N사이의 소수를 구합니다. N은? ");
 	scanf("%d", &N);
-	for (primenumber = 2; primenumber < N; primenumber++) {
-
-		if (N % primenumber == 0)
-			printf("  %d", primenumber);
-	}
-		printf("\n");
-		num = is_prime(num);
-		printf("소수는 모두 %d개입니다.", num);
-	
+	printf("\n");
+	count = is_prime(N);
+	printf("\n");
+	printf("소수는 모두 %d개입니다.", count);
+	*/
 
 
 
