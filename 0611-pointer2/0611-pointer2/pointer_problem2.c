a9ad3cb1b5e2849266d96a48c0769b71b515284f
Kohansol <abc35572003@naver.com>
2020-06-11 22:09:49 +0900

    6번하는중

--- 0611-pointer2/0611-pointer2/pointer_problem2.c
+++ 0611-pointer2/0611-pointer2/pointer_problem2.c
@@ -2,30 +2,63 @@
 #include <stdio.h>
 #define SIZE 10
 
-arith_seq(int arr[SIZE] ,int i,int a) {
-	int *p = arr[SIZE];
-	printf("첫 번째 항? ");
-	scanf("%d", *p);
-	printf("공차? ",a);
-	scanf("%d", a);
-	for (i = 0; i < 10; i++,p++) {
+//5번
+
+//arith_seq(int arr[SIZE] ,int i,int a) {
+//	int *p = &arr[0];
+//	printf("첫 번째 항? ");
+//	scanf("%d", &arr[0]);
+//	printf("공차? ");
+//	scanf("%d", &a);
+//	printf("등차수열:");
+//	for (i = 0; i < 10; i++) {
+//		printf(" %d ", *p);
+//		*p = *p + a;
+//	}
+//	
+//}
+//
+//int main() {
+//
+//	int arr[SIZE] = { 0 };
+//	int i = 0, a = 0;
+//	arith_seq(arr,i,a);
+//
+//	return 0;
+//}
+
+//6번//
+
+get_min_max(int max, int min, int i) {
+	int arr[SIZE] = { 5, 4, 3, 10, 15, 18, 6, 8, 99, 1 };
+	int* p = &arr[0];
+	printf("배열: ");
+	for (i = 0; i < 10; i++, p++) {
 		printf("%d ", *p);
-		*p = *(p + a);
 	}
+	for (i = 0; i < 10; i++, p++){
+		if (*p > *(p+i)) {
+				max = *p;
+				printf("%d \n", max);
+
+			}
+
+			else if (min < *(p + i)) {
+				
+				printf("%d ", min);
+
+			}
+	}
+	printf("\n최대값: ", max);
+	printf("\n");
+	printf("최소값: ", min);
 }
 
 
-
 int main() {
-	int i,j,a,b;
-	int arr[SIZE];
-	
-	arith_seq(arr[SIZE],a,i);
+	int i = 0, max = 0, min = 0;
 
-
-
-
-
+	get_min_max(max, min, i);
 
 	return 0;
 }
